bdb50bad158afabba5cd2ce1ec398af086cb2b22
mmurilo02 <mmurilo02@gmail.com>
2021-11-12 15:24:24 -0300

    Fazendo o botão salvar mostrar a tarefa salva na tela e também fazendo as tarefas salvas aparecerem automaticamente

--- source/js/index.js
+++ source/js/index.js
@@ -16,9 +16,11 @@ let save = document.querySelector('.modal--save')
     save.addEventListener('click', ()=>{
         setListStorage();
         fecharModal();
+        mostrarTarefa();
     })
 
 let arrayTarefas = JSON.parse(localStorage.getItem('dados')) || []
+mostrarTarefasSalvas();
 
 function fecharModal(){
     let description = document.querySelector('.modal--input');
@@ -34,4 +36,28 @@ function setListStorage(){
     arrayTarefas.push(getDescription);
     localStorage.setItem('dados', JSON.stringify(arrayTarefas));
     description.value = '';
+}
+
+function mostrarTarefa(){
+    let tarefa = document.createElement('div');
+    tarefa.className = 'body--lista--tarefa';
+
+    tarefa.innerHTML = '<p class="body--lista--tarefa--nome">' + arrayTarefas[arrayTarefas.length - 1] + '</p>';
+    let divPai = document.getElementsByClassName('body--lista--positionBtnAdd')[0].parentNode;
+    let btnAdd = document.getElementsByClassName('body--lista--positionBtnAdd')[0];
+
+    divPai.insertBefore(tarefa, btnAdd);
+}
+
+function mostrarTarefasSalvas(){
+    for (let tarefaIndice = 0; tarefaIndice < arrayTarefas.length; tarefaIndice++){
+        let tarefa = document.createElement('div');
+        tarefa.className = 'body--lista--tarefa';
+
+        tarefa.innerHTML = '<p class="body--lista--tarefa--nome">' + arrayTarefas[tarefaIndice] + '</p>';
+        let divPai = document.getElementsByClassName('body--lista--positionBtnAdd')[0].parentNode;
+        let btnAdd = document.getElementsByClassName('body--lista--positionBtnAdd')[0];
+
+        divPai.insertBefore(tarefa, btnAdd)
+    }
 }
